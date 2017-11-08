64cddf755bad5e533ffac0239fc177be88f5dc6c
Caleb Kussmaul <calebwk@gmail.com>
2017-11-08 16:52:44 -0500

    Initial Commit

---
+++ Tender/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tender
+//
+//  Created by Caleb Kussmaul on 11/8/17.
+//  Copyright © 2017 Caleb Kussmaul. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Tender/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tender
+//
+//  Created by Caleb Kussmaul on 11/8/17.
+//  Copyright © 2017 Caleb Kussmaul. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
